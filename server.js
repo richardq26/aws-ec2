192bc7ee7bb132fbb2519ad95221ba1bfd906f93
Richard <richard.sw268@gmail.com>
2021-03-20 00:15:30 -0500

    puerto

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get('/', (req,res)=>
     res.send('<h1>Express en EC2</h1>')
 )
 
-app.listen(8080);
+app.listen(3000);
 
-console.log('Server en puerto 8080')
+console.log('Server en puerto 3000');

a20fbcbfec037ba5fb9aa33b3257ce55061bde70
Richard <richard.sw268@gmail.com>
2021-03-19 23:11:09 -0500

    Puerto unico

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express= require('express');
 
 const app = express();
-app.set('port', process.env.PORT || 3000);
+app.set('port', 3000);
 const puerto = app.get('port');
 app.get('/', (req,res)=>{
     res.send('AWS EC2');
